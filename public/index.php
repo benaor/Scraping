b89efa080c13cf8dad9bcfb99b946b4f93d4c518
benaor <benjamingirard25@gmail.com>
2021-02-08 16:10:38 +0100

    create first version of routeur

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
 
+use Router\Router;
+
 require "../vendor/autoload.php";
 
+$router = new Router($_GET['url']);
+
+$router->get('/', 'ScrapingController@index');
+$router->get('/scrap/:id', 'ScrapingController@show');
+
+$router->run();
